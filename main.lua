abd05cbe9b58c056a89cdefdde2076c9afc4fefb
Sergiu Rosu <rsergiu2003@gmail.com>
2011-06-28 20:49:10 +0300

    add license header

--- main.lua
+++ main.lua
@@ -1,3 +1,26 @@
+--[[
+
+	Created by Rosu Sergiu on 27/Jun/2011
+
+	Jump Engine it's a game engine for createing jumping games for mobile devices.
+
+	This file is part of Jump Engine.
+
+    Jump Engine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jump Engine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jump Engine.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright (C) 2011  Rosu Sergiu
+--]]
 --system.setIdleTimer( false );
 display.setStatusBar(display.HiddenStatusBar);
 
@@ -92,3 +115,26 @@ function touch(event)
 	GameManager:touchOnScreen(event);
 end
 Menu.createMenu(Menu);
+
+
+-- add the ads
+
+local adSpace
+function showAd(event)
+	-- Is the url a remote call?
+	if string.find(event.url, "http://", 1, false) == 1 then
+		-- Is it a call to the admob server?
+		if string.find(event.url, "c.admob.com", 1, false) == nil then
+			adSpace.url = event.url
+		else
+			-- an actual click on an ad, so open in Safari
+			system.openURL(event.url)
+		end
+	else
+		print("loading an ad")
+		return true
+	end
+end
+
+adSpace = native.showWebPopup(0, 432, 320, 48, "ad.html", {baseUrl = system.ResourceDirectory, hasBackground = false, urlRequest = showAd})
+
