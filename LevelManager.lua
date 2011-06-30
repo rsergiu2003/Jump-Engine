fdf41b725586bbe0bdea00555661d5e6ee0a24ec
Sergiu Rosu <rsergiu2003@gmail.com>
2011-06-30 23:27:26 +0300

    load remote json and save locally

--- LevelManager.lua
+++ LevelManager.lua
@@ -24,3 +24,70 @@
 
 	Copyright (C) 2011  Rosu Sergiu
 --]]
+
+-- fullLevelData - will hold all the platforms for the level, they will be regulary removed from here and added to the gamemanager's list
+
+LevelManager_ = {requestForLevel="",fullLevelData=""}
+
+LevelManager_.metatable = { __index = LevelManager_ };
+LevelManager_.new = function()
+	local self = {}
+	setmetatable(self, LevelManager_.metatable);
+
+	return self;
+end
+
+LevelManager = LevelManager_.new();
+
+function LevelManager_:LoadLevel(levelName)
+	--first check if we have the file locally
+	local path = system.pathForFile( levelName, system.DocumentsDirectory );
+
+	local file = io.open( path, "r" )
+	if file then
+	   local contents = file:read( "*a" );
+	   io.close( file );
+		print ("level exists localy");
+		LevelManager:ParseLevelData(contents);
+		return true;
+	else
+		if self.requestForLevel ~= "" then
+			print ("level request in progress");
+			return false;
+		end
+		print ("level not found locally");
+		self.requestForLevel = levelName;
+		NetworkWraper:LoadLevel(levelName);
+	end
+	
+	return false;
+end
+
+function LevelManager_:LevelDataArrived(data)
+	print ("remote level data arrived, saving locally on: "..self.requestForLevel);
+	
+	local path = system.pathForFile( self.requestForLevel, system.DocumentsDirectory );
+	file = io.open( path, "w" );
+	file:write(data);
+	io.close( file );
+
+	self.requestForLevel = "";
+	
+	LevelManager:ParseLevelData(data);
+	
+	return true;
+end
+
+-- will parse json level data and add it to it's arrays
+function LevelManager_:ParseLevelData(data)
+	local json = Json.Decode(data);
+	
+	--if we already have a level loaded we need to clear it
+	if self.fullLevelData ~= "" then 
+		print "level already loaded";
+	end
+	
+	self.fullLevelData = {}
+	
+	for index in pairs(json.platform) do print(index) end;
+end
