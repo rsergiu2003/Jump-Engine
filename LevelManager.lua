a7ba1e5e60aa58ccd39d64de0ad0a6496b9ba2c0
Sergiu Rosu <rsergiu2003@gmail.com>
2011-06-30 22:32:23 +0300

    add level manager

---
+++ LevelManager.lua
@@ -0,0 +1,26 @@
+--[[
+
+	Created by Rosu Sergiu on 30/Jun/2011
+
+	Jump Engine it's a game engine for createing jumping games for mobile devices.
+
+	This file is part of Jump Engine.
+	
+	About this file : it's the level manager, will manage the level progress, download the levels, updates to levels, etc.
+	The game mamager will use this class to load levels and inquire about progress
+
+    Jump Engine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jump Engine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jump Engine.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright (C) 2011  Rosu Sergiu
+--]]
